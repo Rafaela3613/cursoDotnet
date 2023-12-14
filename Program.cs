d38f487f7f933356f12e485c9f60da667ef48418
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2023-12-14 14:50:49 -0300

    incrementando calculadora

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
 ﻿using cursoDotnet.Models;
 
-Pessoa pessoa1 = new Pessoa();
+Calculadora calc = new Calculadora();
 
-pessoa1.Nome = "Rafaela";
-pessoa1.Idade = 29;
-pessoa1.Apresentar();
+calc.Somar(10, 30);
+
+calc.Subtrair(15, 5);
+
+calc.Multiplicar(25, 8);
+
+calc.Divisao(46, 12);
+
+
+
+
+
+
+
+
+
+
+
+//Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Nome = "Rafaela";
+//pessoa1.Idade = 29;
+//pessoa1.Apresentar();
