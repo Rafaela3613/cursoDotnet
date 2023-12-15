b40dc498a7dfe0b23468601c12cc09f1fc674a4f
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2023-12-15 11:09:50 -0300

    introduzindo o DO WHILE

--- Program.cs
+++ Program.cs
@@ -1,17 +1,35 @@
 ﻿using cursoDotnet.Models;
 
 
-int numero = 5;
-int contador = 0;
+int soma = 0, numero = 0;
 
-while (contador <= 10){
-          Console.WriteLine($" {contador}° Execucao : {numero} x {contador} = {numero * contador}");
-        contador++;
+do
+{
+    Console.WriteLine("Digite um numero (0 para parar)");
 
-        if(contador == 6){
-        break;
-        }
-}
+    numero = Convert.ToInt32 (Console.ReadLine());
+
+    soma += numero;
+
+} while(numero != 0);
+Console.WriteLine($"Total da soma dos numeros digitados é: {soma}");
+
+
+
+
+
+
+//int numero = 5;
+//int contador = 0;
+
+//while (contador <= 10){
+  //        Console.WriteLine($" {contador}° Execucao : {numero} x {contador} = {numero * contador}");
+    //    contador++;
+
+      //  if(contador == 6){
+        //break;
+        //}
+//}
 
 
 
